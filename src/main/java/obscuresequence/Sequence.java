b86e7b9e6fa7f9b6f17e9326591147f98a45fd8c
OldCurmudgeon <CaswellPaulA@AOL.com>
2017-06-13 09:55:09 +0100

    GPL

--- src/main/java/obscuresequence/Sequence.java
+++ src/main/java/obscuresequence/Sequence.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2017 OldCurmudgeon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package obscuresequence;
 
 import java.math.BigInteger;
@@ -6,7 +24,7 @@ import java.util.Iterator;
 /**
  * Some base methods that all sequences exhibit.
  */
-public abstract class Sequence implements Iterator<BigInteger>{
+public abstract class Sequence implements Iterator<BigInteger> {
 
     /**
      * You cannot remove elements from a sequence.
--- src/test/java/obscuresequence/GaloisLFSRTest.groovy
+++ src/test/java/obscuresequence/GaloisLFSRTest.groovy
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2017 OldCurmudgeon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package obscuresequence
 
 import spock.lang.Shared
@@ -8,7 +26,8 @@ import spock.lang.Specification
  */
 class GaloisLFSRTest extends Specification {
     // Sequence expected from a 3-bit LFSR using the default taps.
-    @Shared correct3BitLFSRSequence = [1, 6, 3, 7, 5, 4, 2]
+    @Shared
+            correct3BitLFSRSequence = [1, 6, 3, 7, 5, 4, 2]
 
     def "Test a simple 3-bit tap"() {
         given: "the default 3-bit tap"
@@ -39,12 +58,11 @@ class GaloisLFSRTest extends Specification {
         where:
         b || r
         // correct3BitLFSRSequence seq with that bit removed.
-        0 || [0,1,3,2]
-        1 || [2,1,3,0]
-        2 || [2,3,1,0]
+        0 || [0, 1, 3, 2]
+        1 || [2, 1, 3, 0]
+        2 || [2, 3, 1, 0]
 
     }
-
 
 
 }
